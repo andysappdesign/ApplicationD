89ea46a88a5b6dac70b9789d8eba197ad697bca9
Andy Cumming <andysappdesign@gmail.com>
2021-05-18 13:08:53 +0100

    Added getWatchList and getObject to MovieWatchList

--- ApplicationD/Core Files/Lists/MovieWatchList.swift
+++ ApplicationD/Core Files/Lists/MovieWatchList.swift
@@ -16,6 +16,7 @@ class MovieWatchList {
     let api = "df8304134d840c4d6d11ca3c0055d5c6"
     let sessionId = UserDefaults.standard.string(forKey: "sessionId")
     let accountId = UserDefaults.standard.string(forKey: "accountId")
+    var listObjectsArray: [[JSON]] = []
     
     // MARK: TESTING ONLY VARIABLES
     
@@ -25,22 +26,44 @@ class MovieWatchList {
     
     // MARK: func getWatchList
     
-    func getWatchList() {
+    private func getWatchList(completionHanderler: @escaping (JSON) -> Void) {
         let url = "https://api.themoviedb.org/3/account/\(String(describing: self.accountId))/watchlist/movies?api_key=\(self.api)&language=en-GB&session_id=\(String(describing: self.sessionId))&sort_by=created_at.asc&page=1"
         
         AF.request(url, method: .get).responseJSON { (responce) in
             switch responce.result {
             case .success(let value):
                 let json = JSON(value)
-                print(json)
+                completionHanderler(json)
             case .failure(let error):
                 print(error)
         
             }
+        } // end of AF.request
+    } // end of getWatchList
+    
+    // MARK: func getObject
+    // to use this command, state a int for the positionNumber of the object you would like
+    // if you would like a random item, enter 99999
+
+    func getObject(positionNumber: Int) {
+        
+        if positionNumber != 99999 {
+            getWatchList { (responce) in
+                if responce != JSON() {
+                    let resultsList = responce["results"].arrayValue
+                    let object = resultsList[positionNumber].arrayValue
+                    self.listObjectsArray.append(object)
+                } else {
+                    print("JSON listObject is empty")
+                    // TODO
+                }
+            } // end of getWatch List
+        } else {
+            print("findiong random position")
+            // TODO
         }
         
-        
-    } // end of getWatchList
+    } // end of getObject
     
     
     
