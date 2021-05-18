d5043802e83361848cb59cf709f4b464eb2fca0b
Andy Cumming <andysappdesign@gmail.com>
2021-05-18 15:51:25 +0100

    updated some of the functions for movieWatchList

--- ApplicationD/Core Files/Lists/MovieWatchList.swift
+++ ApplicationD/Core Files/Lists/MovieWatchList.swift
@@ -16,58 +16,62 @@ class MovieWatchList {
     let api = "df8304134d840c4d6d11ca3c0055d5c6"
     let sessionId = UserDefaults.standard.string(forKey: "sessionId")
     let accountId = UserDefaults.standard.string(forKey: "accountId")
-    var listObjectsArray: [[JSON]] = []
+    var watchListArray: [JSON] = []
     
     // MARK: TESTING ONLY VARIABLES
     
     let TESTapi = "df8304134d840c4d6d11ca3c0055d5c6"
     let TESTsessionId = "3ebe85700311a36a740bb1f8cc4efe7b506cd9f4"
     let TESTaccountId = "9909049"
+ 
+    // MARK:- loadList
     
-    // MARK: func getWatchList
+    func loadList(completionHander: @escaping () -> Void) {
+        getWatchList { (responce) in
+            if responce != JSON() {
+                self.watchListArray = responce["results"].arrayValue
+                let count = self.watchListArray.count
+                print("Watchlist contains \(count) items")
+                print("Downloaded list sucessful")
+                completionHander()
+            } else {
+                print("responce is empty")
+                // TODO
+            }
+        } // end of getWatchList
+    } // end of loadList
     
-    private func getWatchList(completionHanderler: @escaping (JSON) -> Void) {
+    // MARK: - func getWatchList
+    
+    private func getWatchList(completionHandler: @escaping (JSON) -> Void) {
         let url = "https://api.themoviedb.org/3/account/\(String(describing: self.accountId))/watchlist/movies?api_key=\(self.api)&language=en-GB&session_id=\(String(describing: self.sessionId))&sort_by=created_at.asc&page=1"
         
         AF.request(url, method: .get).responseJSON { (responce) in
             switch responce.result {
             case .success(let value):
                 let json = JSON(value)
-                completionHanderler(json)
+                completionHandler(json)
             case .failure(let error):
                 print(error)
-        
             }
         } // end of AF.request
     } // end of getWatchList
     
-    // MARK: func getObject
-    // to use this command, state a int for the positionNumber of the object you would like
-    // if you would like a random item, enter 99999
+    // MARK: - func getObject
 
-    func getObject(positionNumber: Int) {
-        
-        if positionNumber != 99999 {
-            getWatchList { (responce) in
-                if responce != JSON() {
-                    let resultsList = responce["results"].arrayValue
-                    let object = resultsList[positionNumber].arrayValue
-                    self.listObjectsArray.append(object)
-                } else {
-                    print("JSON listObject is empty")
-                    // TODO
-                }
-            } // end of getWatch List
-        } else {
-            print("findiong random position")
-            // TODO
-        }
+    private func getObject(positionNumber: Int) {
+        let object = watchListArray[positionNumber]
+    //    print(object)
+        let posterURL = object["poster_path"].stringValue
+        print(posterURL)
         
     } // end of getObject
     
+    // MARK - getPosterURL
     
+    private func getPosterURL(object: JSON) {
+        
+    }
     
-    
-    
-    
+
 }
--- ApplicationD/Playground.playground/Contents.swift
+++ ApplicationD/Playground.playground/Contents.swift
@@ -5,59 +5,68 @@ import SwiftyJSON
 let api = "df8304134d840c4d6d11ca3c0055d5c6"
 let sessionId = "3ebe85700311a36a740bb1f8cc4efe7b506cd9f4"
 let accountId = "9909049"
+var watchListArray: [JSON] = []
+
+func loadList() {
+    getWatchList { (responce) in
+        if responce != JSON() {
+            watchListArray = responce["results"].arrayValue
+            let count = watchListArray.count
+            print("Watchlist contains \(count) items")
+//            print(watchListArray)
+//            for i in 0..<count {
+//                print(i)
+//                getObject(positionNumber: i)
+//            }
+            let i = 0
+            if i == 0 {
+                print(i)
+                getObject(positionNumber: i)
+            } else {
+                
+            }
+        } else {
+            print("responce is empty")
+            // TODO
+        }
+    } // end of getWatchList
+} // end of loadList
 
 // MARK: func getWatchList
 
-func getWatchList() -> JSON {
+func getWatchList(completionHandler: @escaping (JSON) -> Void) {
     let url = "https://api.themoviedb.org/3/account/\(accountId)/watchlist/movies?api_key=\(api)&session_id=\(sessionId)&sort_by=created_at.asc&page=1"
-    var listObject = JSON()
     
     AF.request(url, method: .get).responseJSON { (responce) in
         switch responce.result {
         case .success(let value):
             let json = JSON(value)
-            //print(json)
-            listObject = json
-            print(listObject)
+            completionHandler(json)
         case .failure(let error):
             print(error)
     
         }
-    }
-    print(listObject)
-    return listObject
-    
+    } // end of AF.request
     
 } // end of getWatchList
 
-func getPoster() {
-    
-} // end of getPoster
 
 // MARK: func getObject
-// to use this command, state a int for the positionNumber of the object you would like
-// if you would like a random item, enter 99999
 
-func getObject(positionNumber: Int) {
-    var listObject = JSON()
-    
-    if positionNumber != 99999 {
-        listObject = getWatchList()
-        if listObject != .null {
-            let resultsObject = listObject["results"].arrayValue
-            print(resultsObject)
-            print(listObject)
-            
-        } else {
-            print("JSON listObject is empty")
-            
-        }
-        
-    } else {
-        print("findiong random position")
-    }
+private func getObject(positionNumber: Int) {
+    let object = watchListArray[positionNumber]
+//    print(object)
+    let posterURL = object["poster_path"].stringValue
+    print(posterURL)
     
 } // end of getObject
 
 
-getObject(positionNumber: 1)
+
+
+private func getPoster() {
+    
+} // end of getPoster
+
+loadList()
+
