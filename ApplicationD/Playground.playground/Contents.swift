6063944f10a8b9a8897d129898d24ff9470b3b60
Andy Cumming <andysappdesign@gmail.com>
2021-05-18 11:59:09 +0100

    organising files

--- ApplicationD/Playground.playground/Contents.swift
+++ ApplicationD/Playground.playground/Contents.swift
@@ -5,27 +5,26 @@ import SwiftyJSON
 let api = "df8304134d840c4d6d11ca3c0055d5c6"
 let sessionId = "3ebe85700311a36a740bb1f8cc4efe7b506cd9f4"
 let accountId = "9909049"
+var listObject = [JSON()]
 
 // MARK: func getWatchList
 
-func getWatchList() -> JSON {
+func getWatchList(completionHandler: @escaping () -> Void) {
     let url = "https://api.themoviedb.org/3/account/\(accountId)/watchlist/movies?api_key=\(api)&session_id=\(sessionId)&sort_by=created_at.asc&page=1"
-    var listObject = JSON()
     
     AF.request(url, method: .get).responseJSON { (responce) in
         switch responce.result {
         case .success(let value):
             let json = JSON(value)
-            //print(json)
-            listObject = json
-            print(listObject)
+            listObject = json["results"].arrayValue
+//            print(listObject)
+            completionHandler()
         case .failure(let error):
             print(error)
+            completionHandler()
     
         }
     }
-    print(listObject)
-    return listObject
     
     
 } // end of getWatchList
@@ -39,25 +38,24 @@ func getPoster() {
 // if you would like a random item, enter 99999
 
 func getObject(positionNumber: Int) {
-    var listObject = JSON()
-    
+
     if positionNumber != 99999 {
-        listObject = getWatchList()
-        if listObject != .null {
-            let resultsObject = listObject["results"].arrayValue
-            print(resultsObject)
-            print(listObject)
-            
-        } else {
-            print("JSON listObject is empty")
-            
+        getWatchList {
+            print("getWatchList complete")
+            if listObject != [JSON()] {
+                print(listObject)
+
+            } else {
+                print("JSON listObject is empty")
+
+            }
         }
         
+
     } else {
         print("findiong random position")
     }
-    
-} // end of getObject
 
+} // end of getObject
 
 getObject(positionNumber: 1)
