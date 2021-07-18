b9ac34546f87e14917870a26375859f250235c4e
Andy Cumming <andysappdesign@gmail.com>
2021-07-18 16:02:16 +0100

    Styled the row

--- ApplicationD/Core Files/Lists/threePosterRow.swift
+++ ApplicationD/Core Files/Lists/threePosterRow.swift
@@ -36,15 +36,23 @@ struct threePosterRow: View {
         self.object3 = JSONObject()
     }
     
+    init(testing: Bool) {
+            self.object1 = JSONObject(id: 1, video: true, original_language: "", overview: "", backdrop_path: "", adult: false, vote_count: 1, vote_average: 1, orginal_title: "", release_date: "", popularity: 0.00, title: "Monsters at Work", poster_path: "2gxgwhcuSmI5xtexb0t9zGj43FS.jpg", genre_ids: [])
+            self.object2 = JSONObject(id: 1, video: true, original_language: "", overview: "", backdrop_path: "", adult: false, vote_count: 1, vote_average: 1, orginal_title: "", release_date: "", popularity: 0.00, title: "WandaVision", poster_path: "glKDfE6btIRcVB5zrjspRIs4r52.jpg", genre_ids: [])
+            self.object3 = JSONObject()
+    }
+    
     let object1: JSONObject
     let object2: JSONObject
     let object3: JSONObject
     
     var body: some View {
         HStack {
+            Spacer()
             VStack {
                 MoviePoster(size: self.size, urlString: object1.poster_path)
             }
+            Spacer()
             VStack {
                 if object2.title != "" {
                     MoviePoster(size: self.size, urlString: object2.poster_path)
@@ -54,6 +62,7 @@ struct threePosterRow: View {
                 }
                 
             }
+            Spacer()
             VStack {
                 if object3.title != "" {
                     MoviePoster(size: self.size, urlString: object3.poster_path)
@@ -62,12 +71,16 @@ struct threePosterRow: View {
                     blankMoviePosition(size: self.size)
                 }
             }
-        }
+            Spacer()
+        } // end of HStack
+        .frame(width: GUISize.rowWidth, height: GUISize.smallPosterHeight + 10)
+        .background(Color(ContentView.colourString))
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
-//struct threePosterRow_Previews: PreviewProvider {
-//    static var previews: some View {
-//        threePosterRow()
-//    }
-//}
+struct threePosterRow_Previews: PreviewProvider {
+    static var previews: some View {
+        threePosterRow(testing: true)
+    }
+}
